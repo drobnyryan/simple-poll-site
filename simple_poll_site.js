ca7f7d2c22033547f6db516facdada338a1a35cf
drobnyryan <166168307+drobnyryan@users.noreply.github.com>
2025-11-05 12:54:23 -0800

    Update simple_poll_site.js
    Fix the feedback link

--- simple_poll_site.js
+++ simple_poll_site.js
@@ -148,7 +148,7 @@ app.get('/', (req, res) => {
     <p>Create a poll and share the link. Polls auto-expire after 90 days.</p>
     <p><a href="/create">Build a Poll!</a></p>
     <p></p>
-    <p><a target="_blank" href="https://pollit.dev/c/kCJ3EUEJbx">Take a Poll!</a> - provide feedback on Poll It!</p>
+    <p><a target="_blank" href="http://pollit.dev/p/wPvOYa-qO5">Take a Poll!</a> - provide feedback on Poll It!</p>
     <hr />
   </div>
 </body>
